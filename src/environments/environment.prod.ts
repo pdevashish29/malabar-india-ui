f8b9ccda124d0a112cbbac637b8b28688ae9466f
pdevashish <devdewance@gmail.com>
2022-07-15 12:21:28 +0530

    load blance base url added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
  // bookPostBaseUrl:'https://blooming-mountain-87762.herokuapp.com'
- bookPostBaseUrl:'https://calm-brook-04823.herokuapp.com'
+ //bookPostBaseUrl:'https://calm-brook-04823.herokuapp.com'
+ bookPostBaseUrl:'https://darauli.in'
 };
