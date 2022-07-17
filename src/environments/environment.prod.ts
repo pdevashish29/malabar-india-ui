1093e49336930f67314aa2fd43fa0ac9d3e0e75e
pdevashish <devdewance@gmail.com>
2022-07-17 09:40:21 +0530

    Backend service changed to prodution

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
- // bookPostBaseUrl:'https://blooming-mountain-87762.herokuapp.com'
- //bookPostBaseUrl:'https://calm-brook-04823.herokuapp.com'
-  // below URL is globar server load blanced enbaled 
- bookPostBaseUrl:'https://darauli.in'
+  bookPostBaseUrl:'https://calm-brook-04823.herokuapp.com'
 };
