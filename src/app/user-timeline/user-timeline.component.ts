75edf85eac6949614dc2f5515796970e50763489
pdevashish <devdewance@gmail.com>
2022-07-20 02:12:27 +0530

    timeline page and users page ui updated

--- src/app/user-timeline/user-timeline.component.ts
+++ src/app/user-timeline/user-timeline.component.ts
@@ -12,16 +12,23 @@ import { BookPostService } from '../service/book-post.service';
 export class UserTimelineComponent implements OnInit {
 
   userTimeLine:any;
-
+  loading:boolean=false;
+  isError:boolean=false;
   constructor(private postBookService:BookPostService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-
+    this.loading=true;
+    this.isError=false;
     this.userTimeLine = this.route.paramMap.subscribe(params =>{
        let id =  params.get('id');
        this.postBookService.getUserTimeLine(id+'').subscribe(data => {
+          this.loading=false;
           console.log(data)
           this.userTimeLine=data;
+        }, error => {
+          this.loading=false;
+          this.isError=true;
+          console.log(error);
         })
     });
   
